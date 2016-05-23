62661f970490902f794c839012005a7f8127e535
Gregivy <gpoi@mail.ru>
2016-05-23 23:45:32 +0300

    final

--- pages/login.js
+++ pages/login.js
@@ -51,6 +51,7 @@ var login = new tabris.Button({
 
 function checksuccess() {
 	fetch("./scripts/checklogin.js?_="+Math.random(),{method:"get",cache:"no-cache"}).then(function(response) {
+		console.log(response);
   		return response.text();
 	}).then(function(text) {
 		var ref = cordova.InAppBrowser.open('http://ekim.ru', '_blank', iabSettings);
@@ -83,7 +84,8 @@ login.on("select", function() {
 	switchBusy();
 	localStorage.setItem("username",username.get("text"));
 	localStorage.setItem("password",password.get("text"));
-	fetch("./scripts/login.js?_="+Math.random(),{method:"get",cache:"no-cache"}).then(function(response) {
+	fetch("file:///android_asset/www/scripts/login.js?_="+Math.random(),{method:"get",cache:"no-cache"}).then(function(response) {
+		console.log(response);
   		return response.text();
 	}).then(function(text) {
 		console.log(text);
