da0ca61144e1fb1eec60acc3ed6dc246426b7132
Gregivy <gpoi@mail.ru>
2016-05-24 14:15:29 +0300

    final

--- pages/login.js
+++ pages/login.js
@@ -84,7 +84,7 @@ login.on("select", function() {
 	switchBusy();
 	localStorage.setItem("username",username.get("text"));
 	localStorage.setItem("password",password.get("text"));
-	fetch("file:///android_asset/www/scripts/login.js?_="+Math.random(),{method:"get",cache:"no-cache"}).then(function(response) {
+	fetch("./scripts/login.js?_="+Math.random(),{method:"get",cache:"no-cache"}).then(function(response) {
 		console.log(response);
   		return response.text();
 	}).then(function(text) {
